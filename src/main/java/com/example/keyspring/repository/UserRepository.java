b76791d8310ff808bfb55013af5f9688cb79104c
Arthur Artugue <artuguearthur@gmail.com>
2024-12-22 16:59:13 +0800

    added user repository interface

---
+++ src/main/java/com/example/keyspring/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.example.keyspring.repository;
+
+import com.example.keyspring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.uniqueId = ?1")
+    Optional<User> findByUniqueId(String uniqueId);
+
+}
